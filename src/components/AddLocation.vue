ba8ff3801c86e7f8532a9d1f53d2f65c2f402215
sujaydas <sujaykumardas16111993@gmail.com>
2018-01-22 15:28:19 +0530

    Added admin addlocation

---
+++ src/components/AddLocation.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h1>Add A Location</h1>
+    <form>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Location Name:</label>
+            <input type="text" class="form-control" v-model="location.name">
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Location Address:</label>
+            <input type="text" class="form-control col-md-6" v-model="location.address">
+          </div>
+        </div>
+      </div><br />
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Nearby Location Address:</label>
+            <input type="text" class="form-control col-md-6" v-model="location.nearby_address">
+          </div>
+        </div>
+      </div><br />
+      <div class="form-group">
+        <button class="btn btn-primary" @click="addLocation()">Add Location</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default{
+	data(){
+		return {
+			location: {
+				name: '',
+				address: '',
+        nearby_address: ''
+			}
+		}
+	},
+	methods:{
+		addLocation: function(){
+			let uri = 'http://localhost:4000/locations/add';
+        this.axios.post(uri, this.location).then((response) => {
+          this.$router.push({name: 'HomePage'});
+        });
+      this.$router.push({name: 'HomePage'});
+		}
+	}
+}
+
+</script>
