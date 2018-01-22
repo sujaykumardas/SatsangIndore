8be7cd56567b76eb7f5ea5685fe24d9514ae3488
sujaydas <sujaykumardas16111993@gmail.com>
2018-01-22 15:31:55 +0530

    Added Location modal

---
+++ models/Location.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// Define collection and schema for Items
+var Location = new Schema({
+  name: {
+    type: String
+  },
+  address: {
+    type: String
+  },
+  nearby_address: {
+    type: String
+  },
+
+},{
+    collection: 'locations'
+});
+
+module.exports = mongoose.model('Location', Location);
